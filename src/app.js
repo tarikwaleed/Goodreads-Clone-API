48e929f76d5f57ee17b023cdf66453744fcdc48d
tarikwaleed <takizee53@gmail.com>
2023-03-07 17:51:39 +0200

    :construction: database connected successfully

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("./config/database").connect()
 
 //! Here
 var indexRouter = require('./routes/index.route');
---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
+
+const {LOCAL_DB_URI} = process.env
+const { REMOTE_DB_URL } = process.env
+
+exports.connect = () => {
+    // Connecting to the database
+    mongoose
+        .connect(LOCAL_DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("Successfully connected to database")
+        })
+        .catch((error) => {
+            console.log("database connection failed. exiting now...")
+            console.error(error)
+            process.exit(1)
+        })
+}
