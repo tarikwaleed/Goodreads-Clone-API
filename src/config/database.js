c1d793d7fbc6fef486c175d070db1896543a1c16
YoussefMohamed101 <yfathy20920@gmail.com>
2023-03-11 07:02:15 +0200

    merging

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,25 @@
-require("dotenv").config()
-const mongoose = require("mongoose")
-mongoose.set("strictQuery", false)
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-const { LOCAL_DB_URI } = process.env
-const { REMOTE_DB_URI } = process.env
+const { LOCAL_DB_URI } = process.env;
+const { REMOTE_DB_URI } = process.env;
 
+const url =
+  "mongodb+srv://Database_Admins:yH6ocwxKojzGgWqn@goodreads-cluster.dqgigbj.mongodb.net/Goodreads-Database-0?retryWrites=true&w=majority";
 exports.connect = () => {
-    // Connecting to the database
-    mongoose
-        .connect(REMOTE_DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("Successfully connected to database")
-        })
-        .catch((error) => {
-            console.log("database connection failed. exiting now...")
-            console.error(error)
-            process.exit(1)
-        })
-}
+  // Connecting to the database
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
