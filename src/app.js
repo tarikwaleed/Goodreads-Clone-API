65ca2c087b8efe1dc504f321c2ba5ec0b5a334dc
tarikwaleed <takizee53@gmail.com>
2023-03-17 23:29:53 +0200

    added cors dependency

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("./config/database").connect();
-// const cors = require('cors');
+const cors = require('cors');
 
 var indexRouter = require("./routes/index.route");
 var bookRouter = require("./routes/book.route");
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "public")));
-// app.use(cors());
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api/book", bookRouter);
