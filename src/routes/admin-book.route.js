a590d1896a5ef635692df0470885e1dbfc5fe1c2
YoussefMohamed101 <yfathy20920@gmail.com>
2023-04-01 23:58:44 +0200

    fixing bugs

--- src/routes/admin-book.route.js
+++ src/routes/admin-book.route.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const auth = require('../middlewares/auth');
-const bookController = require('../controllers/book.controller');
-const upload = require('../middlewares/image_upload');
-const multer = require('multer');
+const express = require("express");
+const auth = require("../middlewares/auth");
+const bookController = require("../controllers/book.controller");
+const upload = require("../middlewares/image_upload");
+const multer = require("multer");
 
 const router = express.Router();
 
-router.post('/', upload.single('coverImage'), bookController.book_create);
-router.put('/', upload.single('coverImage'), bookController.book_update);
-router.delete('/:id',  bookController.book_delete);
+router.post("/", upload.single("coverImage"), bookController.book_create);
+router.put("/", upload.single("coverImage"), bookController.book_update);
+router.delete("/:id", bookController.book_delete);
 
 module.exports = router;
